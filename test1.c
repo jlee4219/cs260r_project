53e0d924598b9228fcf73cecbdd393bb885c3f82
Jefferson Lee <jeffersonlee@college.harvard.edu>
2015-04-12 21:38:41 -0400

    Added a new example, generated new results.

---
+++ test1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <assert.h>
+#define NUM_THREADS 20
+#define NUM_INC 400
+
+int count = 0;
+
+void* f (void* arg) {
+  int i = 0;
+  for (i; i < NUM_INC; ++i){
+    ++count;
+  }
+}
+
+int main () {
+  pthread_t threads[NUM_THREADS];
+  int i = 0;
+  for (i; i < NUM_THREADS; ++i){
+    pthread_create(threads+i, NULL, &f, NULL);
+  }
+  for(i = 0; i < NUM_THREADS; ++i){
+    pthread_join(threads[i], NULL);
+  }
+  assert(count == NUM_THREADS * NUM_INC);
+  return 0;
+}
