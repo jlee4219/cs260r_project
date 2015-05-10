2b7b2114654cd74b1b0d1729b69aa7199d21d989
Jefferson Lee <jeffersonlee@college.harvard.edu>
2015-05-10 18:15:08 -0400

    handle time stamp, local context changes

--- recon.cpp
+++ recon.cpp
@@ -11,8 +11,11 @@
 #define LCL_W 1
 #define RMT_R 2
 #define RMT_W 3
+#define CTXT_L 5
 // #define TABLE_SIZE 16777216
 #define TABLE_SIZE 1000
+int count = 0;
+static TLS_KEY tls_key;
 
 // class for a node in context-aware communication graph
 class NODE{
@@ -22,9 +25,9 @@ public:
     int threadid;
     int time_stamp;
     NODE* next;
-    NODE(unsigned long ins_p, int t_id, int stamp){
+    NODE(unsigned long ins_p, int t_id, int stamp, int ctxt){
         ip = ins_p;
-        context = 0;
+        context = ctxt;
         threadid = t_id;
         time_stamp = stamp;
         next = NULL;
@@ -36,17 +39,20 @@ public:
     NODE last_writer;
     NODE* sharers_list;
 
-    void update_writer(unsigned long ins_p, int t_id, int stamp) {
+  void update_writer(unsigned long ins_p, int t_id, int stamp, int ctxt) {
         last_writer.ip = ins_p;
         last_writer.threadid = t_id;
         last_writer.time_stamp = stamp;
+        last_writer.context = ctxt;
         sharers_list = NULL;
+        count += 1;
     }
 
-    void add_reader(unsigned long ins_p, int t_id, int stamp) {
-        NODE* reader = new NODE(ins_p, t_id, stamp);
+  void add_reader(unsigned long ins_p, int t_id, int stamp, int ctxt) {
+    NODE* reader = new NODE(ins_p, t_id, stamp, ctxt);
         reader->next = sharers_list;
         sharers_list = reader;
+        count += 1;
     }
 
     bool has_read(int t_id){
@@ -59,7 +65,8 @@ public:
         return false;
     }
 
-  HashEntry(unsigned long ins_p, int t_id, int stamp): last_writer(ins_p, t_id, stamp), sharers_list(NULL) { }
+  HashEntry(unsigned long ins_p, int t_id, int stamp, int ctxt):
+    last_writer(ins_p, t_id, stamp, ctxt), sharers_list(NULL) { }
 };
 
 class HashMap {
@@ -90,13 +97,18 @@ public:
     }
 
     void print_edges(HashEntry* entry){
-        trace_stream << (entry->last_writer).ip << " " << (entry->last_writer).threadid;
+        print_node(&(entry->last_writer));
         NODE* cur = entry->sharers_list;
 	while (cur != NULL){
-	    trace_stream << " " << cur->ip << " " << cur->threadid;
+	    trace_stream << " ";
+	    print_node(cur);
 	    cur = cur -> next;
         }
         trace_stream << endl;
+    }
+
+    void print_node(NODE* node){
+      trace_stream << node->ip << " " << node->threadid << " " << node->time_stamp << " " << node->context;
     }
 
     void close_stream(){
@@ -112,29 +124,63 @@ public:
     }
 };
 
+struct ins_type {
+  short type;
+  ins_type* next;
+  ins_type* prev;
+};
+
+class Context {
+private:
+    ins_type* head;
+public:
+    Context() {
+        head = (ins_type*) malloc(sizeof(ins_type) * CTXT_L);
+        for (int i = 0; i < CTXT_L; ++i){
+            (head+i)->next = head + ((i + 1) % CTXT_L);
+            (head+i)->prev = head + ((CTXT_L - 1) % CTXT_L);
+            (head+i)->type = -1;
+        }
+    }
+
+    void add(short type){
+        head = head -> prev;
+        head->type = type;
+    }
+
+    int to_int(){
+        ins_type* cur = head;
+        int accum = 0;
+        for (int i = 0; i < CTXT_L; ++i){
+	    accum *= 4;
+            if (cur -> type != -1)
+	        accum += cur -> type;
+        }
+        return accum;
+    }
+};
+
+Context* get_tls(THREADID threadid)
+{
+    Context* ctxt = static_cast<Context*>(PIN_GetThreadData(tls_key, threadid));
+    return ctxt;
+}
+
 // FILE * trace;
 PIN_LOCK thread_lock;
 HashMap* metadata = new HashMap();
-// unordered_map <string, NODE> metadata;
-
-
-/*string IPToString(VOID * ip){
-    string ins;
-    stringstream strstream;
-    strstream << (long) ip;
-    strstream >> ins;
-    return ins;
-}*/
 
 // Print a memory read record
 VOID RecordMemRead(VOID * ip, VOID * addr, THREADID threadid)
 {
+    Context* ctxt = get_tls(threadid);
+    ctxt->add(LCL_R);
     HashEntry* entry = metadata->get((unsigned long) addr);
     if (entry != NULL){
       if (!entry->has_read((int) threadid) && (int)threadid != (entry->last_writer).threadid)
 	{
             PIN_GetLock(&thread_lock, threadid+1);
-            entry->add_reader((unsigned long) ip, threadid, 0);
+            entry->add_reader((unsigned long) ip, threadid, count, ctxt->to_int());
             PIN_ReleaseLock(&thread_lock);
         }
     }
@@ -143,19 +189,21 @@ VOID RecordMemRead(VOID * ip, VOID * addr, THREADID threadid)
 // Print a memory write record
 VOID RecordMemWrite(VOID * ip, VOID * addr, THREADID threadid)
 {
+    Context* ctxt = get_tls(threadid);
+    ctxt->add(LCL_W);
     HashEntry* entry = metadata->get((unsigned long) addr);
     if (entry != NULL){
         if ((entry -> last_writer).threadid != (int) threadid)
 	{
 	    PIN_GetLock(&thread_lock, threadid+1);
             metadata->print_edges(entry);
-	    entry->update_writer((unsigned long) ip, (int) threadid, 0);
+	    entry->update_writer((unsigned long) ip, (int) threadid, count, ctxt->to_int());
 	    PIN_ReleaseLock(&thread_lock);
         }
     }
     else{
         PIN_GetLock(&thread_lock, threadid+1);
-        HashEntry* write = new HashEntry((unsigned long) ip,(int) threadid, 0);
+        HashEntry* write = new HashEntry((unsigned long) ip,(int) threadid, count, ctxt->to_int());
         metadata->put((unsigned long) addr, write);
         PIN_ReleaseLock(&thread_lock);
     }
@@ -194,7 +242,7 @@ VOID ImageLoad(IMG img, VOID *v)
                     // Iterate over each memory operand of the instruction.
                     for (UINT32 memOp = 0; memOp < memOperands; memOp++)
                     {
-                        if (INS_MemoryOperandIsRead(ins, memOp))
+		      if (INS_MemoryOperandIsRead(ins, memOp) && !INS_IsStackRead(ins))
                         {
                             INS_InsertPredicatedCall(
                                 ins, IPOINT_BEFORE, (AFUNPTR)RecordMemRead,
@@ -206,7 +254,7 @@ VOID ImageLoad(IMG img, VOID *v)
                         // Note that in some architectures a single memory operand can be 
                         // both read and written (for instance incl (%eax) on IA-32)
                         // In that case we instrument it once for read and once for write.
-                        if (INS_MemoryOperandIsWritten(ins, memOp))
+		      if (INS_MemoryOperandIsWritten(ins, memOp) && !INS_IsStackWrite(ins))
                         {
                             INS_InsertPredicatedCall(
                                 ins, IPOINT_BEFORE, (AFUNPTR)RecordMemWrite,
@@ -237,6 +285,12 @@ INT32 Usage()
     return -1;
 }
 
+// Note: the other context here is a pin thing.
+VOID ThreadStart(THREADID threadid, CONTEXT* ct, INT32 flags, VOID* v){
+    Context* ctxt = new Context;
+    PIN_SetThreadData(tls_key, ctxt, threadid);
+}
+
 VOID Fini(INT32 code, VOID* v){
     metadata->print_table();
     metadata->close_stream();
@@ -255,8 +309,13 @@ int main(int argc, char * argv[])
     // Initialize pin
     if (PIN_Init(argc, argv)) return Usage();
 
+    tls_key = PIN_CreateThreadDataKey(0);
+
     // Register ImageLoad to be called when an image is loaded
     IMG_AddInstrumentFunction(ImageLoad, 0);
+
+    PIN_AddThreadStartFunction(ThreadStart, 0);
+
     PIN_AddFiniFunction(Fini, 0);
 
     // Start the program, never returns
